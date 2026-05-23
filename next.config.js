363d42a987b05d625f497a2afdde7c83857c52c2
sagheer kaifi <kaifisageer@gmail.com>
2026-05-23 16:30:06 +0530

    fix: remove experimental outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
   productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
